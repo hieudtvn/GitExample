4760f0c7fc300b9b2fbc0f5e8d22bbad62857bf1
Hieu Dao <hieudtvn@gmail.com>
2019-03-07 11:02:50 +0700

    a

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -13,6 +13,7 @@ namespace GitExample
             Console.Write("Nhap so thu nhat: ");
             string s = Console.ReadLine();
             Console.WriteLine("So thu nhat la: " + s);
+            Console.WriteLine("So thu nhat la: " + s);
             Console.ReadLine();
         }
     }
